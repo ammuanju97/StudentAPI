7d20087a25765f6b0ddef0d9db964c914a91367b
ammuanju97 <anjutennyson@gmail.com>
2022-02-24 10:38:03 +0530

    student registration and login donegit add .

--- student/settings.py
+++ student/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+from decouple import config
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l(3y)nr542x1do_==gkfd9b6lb&o270w(fd#2_1y8jwi1g%fix'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
